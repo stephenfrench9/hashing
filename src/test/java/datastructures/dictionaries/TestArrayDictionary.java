de498bbeb15bf73be473456443619b29db01d9a5
Stephen French <stephenfrench9@gmail.com>
2018-11-05 19:24:26 -0800

    write an iterator for you array dictionary

--- src/test/java/datastructures/dictionaries/TestArrayDictionary.java
+++ src/test/java/datastructures/dictionaries/TestArrayDictionary.java
@@ -2,9 +2,16 @@ package datastructures.dictionaries;
 
 import datastructures.concrete.dictionaries.ArrayDictionary;
 import datastructures.interfaces.IDictionary;
+import org.junit.Test;
 
 public class TestArrayDictionary extends TestDictionary {
     protected <K, V> IDictionary<K, V> newDictionary() {
         return new ArrayDictionary<>();
     }
+
+    @Test
+    public void testIterator() {
+
+    }
+
 }
